de732549c917ffc3ffbee664816ef31904168b75
samir jadhav <samirzjadhav@gmail.com>
2024-12-27 17:24:55 +0530

    fix: finally fixed a problem for hover is not working propary

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -34,60 +34,63 @@ const Hero = () => {
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
       >
-        <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-          <div
-            className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
-            onClick={handleMiniVdClick}
-          >
-            <video
-              ref={nextVideoRef}
-              src={videoRefs[upcommingVideoIndex]} // Get the video dynamically based on currentIndex
-              loop
-              muted
-              autoPlay
-              type="video/mp4"
-              id="current-video"
-              className="size-64 object-cover object-center scale-150 object-center"
-              onError={() => console.error("Video failed to load")}
-              onLoadedData={handleVidoeLoad}
+        <div className="">
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <div
+              className="origin-center scale-50 opacity-0 transition-all  duration-500 ease-in hover:scale-100 hover:opacity-100"
+              onClick={handleMiniVdClick}
+            >
+              <video
+                ref={nextVideoRef}
+                src={videoRefs[upcommingVideoIndex]} // Get the video dynamically based on currentIndex
+                loop
+                muted
+                autoPlay
+                type="video/mp4"
+                id="current-video"
+                className="size-64 object-cover object-center scale-150 "
+                onError={() => console.error("Video failed to load")}
+                onLoadedData={handleVidoeLoad}
+              />
+            </div>
+          </div>
+          <video
+            src={videoRefs[currentIndex]}
+            ref={nextVideoRef}
+            loop
+            muted
+            id="next-video"
+            className=" absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            onLoadedData={handleVidoeLoad}
+          />
+          <video
+            src={videoRefs[currentIndex === totalVideos + 1 ? 1 : currentIndex]}
+            autoPlay
+            loop
+            muted
+            className="absolute left-0 right-0 z-10 size-full object-cover object-center"
+            onLoadedData={handleVidoeLoad}
+          />
+        </div>
+
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75 uppercase">
+          G<b>a</b>Ming
+        </h1>
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100 uppercase">
+              redefi<b>n</b>e
+            </h1>
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the Metagame Layer <br /> Unleash the Play Economy
+            </p>
+            <Button
+              id="watch-trailer"
+              title="Watch Trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="!bg-yellow-300 flex-center gap-1"
             />
           </div>
-        </div>
-        <video
-          src={videoRefs[currentIndex]}
-          ref={nextVideoRef}
-          loop
-          muted
-          id="next-video"
-          className=" absolute-center invisible absolute z-20 size-64 object-cover object-center"
-          onLoadedData={handleVidoeLoad}
-        />
-        <video
-          src={videoRefs[currentIndex === totalVideos + 1 ? 1 : currentIndex]}
-          // autoPlay
-          loop
-          muted
-          className="absolute left-0 right-0 z-10 size-full object-cover object-center"
-          onLoadedData={handleVidoeLoad}
-        />
-      </div>
-      <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75 uppercase">
-        G<b>a</b>Ming
-      </h1>
-      <div className="absolute left-0 top-0 z-40 size-full">
-        <div className="mt-24 px-5 sm:px-10">
-          <h1 className="special-font hero-heading text-blue-100 uppercase">
-            redefi<b>n</b>e
-          </h1>
-          <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
-            Enter the Metagame Layer <br /> Unleash the Play Economy
-          </p>
-          <Button
-            id="watch-trailer"
-            title="Watch Trailer"
-            leftIcon={<TiLocationArrow />}
-            containerClass="!bg-yellow-300 flex-center gap-1"
-          />
         </div>
       </div>
     </div>
